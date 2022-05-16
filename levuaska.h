b59bb2c93b859b4f6bfa0d7ede82e8a522eeb5fd
leo <80543539+justleoo@users.noreply.github.com>
2022-05-16 18:48:13 -0300

    add other form.

---
+++ levuaska.h
@@ -0,0 +1,35 @@
+/* 
+* name: Levuaska
+* github: @levuaska
+*/
+
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"#13141c",
+	"#d78787",
+	"#afbea2",
+	"#e4c9af",
+	"#a1bdce",
+	"#d7beda",
+	"#b1e7dd",
+	"#bfc9db",
+
+	/* 8 bright colors */
+	"#646a73",
+	"#d78787",
+	"#afbea2",
+	"#e4c9af",
+	"#a1bdce",
+	"#d7beda",
+	"#b1e7dd",
+	"#858893",
+
+[256] = "#bfc9db", // foreground
+[257] = "#0f0f17", // background
+[258] = "#bfc9db",
+};
+
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
